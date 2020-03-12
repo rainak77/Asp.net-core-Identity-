cdba34e1d24f6a091f4138244cdcabe2e798c9d3
md syedul arif <raiak_77@yahoo.com>
2020-03-12 12:26:02 +0200

    Add project files.

---
+++ Users/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser()
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach(IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Users/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Dictionary<string, object> {
+
+                ["placeholder"] = "PlaceHolder" 
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
